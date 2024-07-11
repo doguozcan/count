f768faed134bab512c7c62f885460edff859d52b
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 10:58:03 +0300

    Add daisyUI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="text-5xl">Count</div>
+  <div>
+    <p class="text-xl">Count</p>
+    <button class="btn">Decrement by 1</button>
+    <button class="btn">Increment by 1</button>
+  </div>
 </template>
 
 <script setup></script>
