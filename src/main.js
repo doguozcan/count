507df3452b62549b278a76155483fecc4694653d
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 10:59:57 +0300

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import './assets/index.css'
 
-createApp(App).mount('#app')
+const store = createStore({})
+
+const app = createApp(App)
+
+app.use(store)
+app.mount('#app')
