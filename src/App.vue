c82d41215ab47aac9a58d5d1ad7d926c964545b9
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 11:22:41 +0300

    Add increment and decrement randomly actions using random.org's api

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <p class="text-xl">{{ $store.state.counter }}</p>
-    <button class="btn" @click="$store.commit('decrementBy1')">
+    <button class="btn" @click="$store.commit('decrement')">
       Decrement by 1
     </button>
-    <button class="btn" @click="$store.commit('incrementBy1')">
+    <button class="btn" @click="$store.commit('increment')">
       Increment by 1
+    </button>
+    <button class="btn" @click="$store.dispatch('decrementRandomly')">
+      Decrement randomly
+    </button>
+    <button class="btn" @click="$store.dispatch('incrementRandomly')">
+      Increment randomly
     </button>
   </div>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,32 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
     counter: 0,
   },
   mutations: {
-    incrementBy1(state) {
-      state.counter += 1
+    decrement(state, data = 1) {
+      state.counter -= data
     },
-    decrementBy1(state) {
-      state.counter -= 1
+    increment(state, data = 1) {
+      state.counter += data
     },
   },
-  actions: {},
+  actions: {
+    decrementRandomly({ commit }) {
+      axios(
+        'https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new'
+      ).then((value) => {
+        commit('decrement', value.data)
+      })
+    },
+    incrementRandomly({ commit }) {
+      axios(
+        'https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new'
+      ).then((value) => {
+        commit('increment', value.data)
+      })
+    },
+  },
 })
